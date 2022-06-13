73163ced0fd56946b6c8370c24e58a8f39e43a1e
kamalesh <kamalesh0399@gmail.com>
2022-06-13 21:51:07 +0530

    Added Binary_search

---
+++ Binary_search.py
@@ -0,0 +1,26 @@
+def binary_search(list, target):
+    first =0
+    last = len(list) -1
+
+    while first <= last:
+        midpoint = (first + last)//2 # floor division 
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint -1
+    return None
+
+def verfy (index):
+    if index is not None:
+        print("Taget found at:", index)
+    else:
+        print("Target not found")
+
+
+list1= [1,2,3,3,4,9,0,5,6,7,9]
+result = (binary_search(list1, 3))
+
+verfy(result)
